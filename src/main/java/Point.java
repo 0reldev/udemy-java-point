7d614695063ee5b9f1c55fd4eb0eb9a95896c855
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-04-04 16:32:43 +0200

    Exercise solution

---
+++ src/main/java/Point.java
@@ -0,0 +1,41 @@
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return distance(0, 0);
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((double) ((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y)));
+    }
+
+    public double distance(Point point) {
+        return distance(point.x, point.y);
+    }
+}
